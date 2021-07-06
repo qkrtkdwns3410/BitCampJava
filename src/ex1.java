3d70233e8c8210a203eecd6fe405468902ad849d
psj <qkrtkdwns3410@naver.com>
2021-07-06 15:09:05 +0900

    7-6

--- src/ex1.java
+++ src/ex1.java
@@ -1,5 +1,35 @@
+//라인 주석
+/*
+블록주석 (해석되지 않는 자원)
+일반적으로 만든날짜 : 2021 - 7 - 개더운 여름에...
+만든이 : 박상준
+목적 : class 사용방법
+
+public class Ex01 >>Ex01 클래스 >> 설계도 >> 데이터 타입..(숫자, 문자, 날짜 etc) :사용자가 만드는 데이터 타입입니다.
+클래스 == 설계도 == 타입(여러개의 작은 타입을 모아놓은 것입니다)
+
+종류 :
+1.class Test { } >> 독자적인 실행이 불가능한 설계도 입니다. >> 다른 클래스를 도와주는 클래스를 만드는 것입니다.
+					>>메인 함수가 없습니다. >> lirary라고 합니다. (줄여서 lib ...)
+2.class Test{public static void main(String[] args{} } >> 독자적인 실행이 가능한 클래스입니다.
+2.1  statice void main(String[] args) >> main 이라는 이름을 가지고 함수 ,static >>
+        >>프로그램 시작점 , 진입점 >> 실행가능합니다. >> 약속입니다!!!!!!
+
+Tip >
+C#> static void Main(){}
+-- 한달 동안 배우고 서점에서 C#책을 열어보면 대부분 비슷.
+
+변수 : variable
+데이터(자료) 담을 수 있는 공간에 이름을...
+변수는 공간의 크기를 가져야합니다.(데이터 타입으로 정의합니다)
+데이터 타입      변수명     >> int i =100; (4바이트 공간에 이름을 i로 하며, 데이터를 100을 채우겠다)
+
+
+ */
 public class ex1 {
+
     public static void main(String[] args) {
-        System.out.println("**");
+        System.out.println("*");
     }
+
 }
