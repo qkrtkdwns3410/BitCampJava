dd1162e902332b279cf278f82c00a10c7d70a4dd
psj <qkrtkdwns3410@naver.com>
2021-07-06 19:09:46 +0900

    7-6

---
+++ src/javaBasic/ch6_1_OOP.java
@@ -0,0 +1,20 @@
+package javaBasic;
+
+/*
+객체지향 언어 = 프로그래밍언어 + 객체지향개념(규칙)
+1.코드의 재사용성을 높이고 , 유지보수가 용이해집니다.
+2.코드의 중복을 제거합니다.
+
+OOP의 요소 (캡상추다)
+1.캡슐화
+2.상속
+3.추상화
+4.다형성
+
+
+ */
+public class ch6_1_OOP {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/javaBasic/ch6_2_4_클래스의정의.java
@@ -0,0 +1,7 @@
+package javaBasic;
+
+public class ch6_2_4_클래스의정의 {
+    public static void main(String[] args) {
+
+    }
+}
