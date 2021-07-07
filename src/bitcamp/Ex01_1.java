c69d41fde082c9d4796f1398de75d333d510d9d0
psj <qkrtkdwns3410@naver.com>
2021-07-07 17:56:18 +0900

    7-6

--- src/bitcamp/Ex01_1.java
+++ src/bitcamp/Ex01_1.java
@@ -19,7 +19,7 @@ car2
 car객체 >>stack영역에서 car2객체의 주소를 참조합니다.  (주소 yy)
 
 car3
-각 지역변수마다 4바이트의 공간을 할당합니다. >> new Car() 하는 순간 힙영역에 Car객체를 만들고  int window라는 객체변수를 생성하고 void move()라는 성능관련 메서드가 생성합니다.
+각 지역변수마다 4바이트의 공간을 할당합니다. >> new Car() 하는 순간 """"""힙영역""""""""에 Car객체를 만들고  int window라는 객체변수를 생성하고 void move()라는 성능관련 메서드가 생성합니다.
 >>
 car객체 >>stack영역에서 car 주소를 참조합니다.  (주소 zz)
 
@@ -30,9 +30,25 @@ car1. "".""의 의미는 참조연산자의 의미입니다.
 
 public class Ex01_1 {
     public static void main(String[] args) {
+
         Car car1 = new Car();
         Car car2 = new Car();
         Car car3 = new Car();
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/javaBasic/ch6/ch6_9_10_class.java
+++ src/javaBasic/ch6/ch6_9_10_class.java
@@ -1,5 +1,7 @@
 package javaBasic.ch6;
 
+import java.util.Scanner;
+
 /*
 클래스
 
@@ -11,7 +13,65 @@ package javaBasic.ch6;
 *배열 : 같은 종류의 여러 데이터를 하나로 저장할 수 있는 공간
 *구조체 : 서로 관련된 여러 데이터    (변수의 자료형 관계 X)      를 하나로 저장 할 수 있는 공간
 *클래스 : 데이터와 함수의 결합 (구조체 + 함수) >>함수과 관련있는 데이터 ..묶음..
+
+클래스 : 사용자 정의 타입 : 원하는 타입을 직접 만들 수 있습니다.
+int hour, int minute , int second --- 등 ...
+
+시간 과 분 초 타입을 묶어서 독립적인 class
  */
+class Time {
+    private  int hour;
+    private  int minute;
+    private  int second;
+
+    public Time() { //기본 생성자
+
+    }
+
+    public Time(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+}
 
 public class ch6_9_10_class {
+    public static void main(String[] args) {
+//        int hour_I;
+//        int minute_I;
+//        int second_I;
+//        Time t = new Time();
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("시간을 입력하세요:");
+//        hour_I = sc.nextInt();
+//        System.out.println("분을 입력하세요:");
+//        minute_I = sc.nextInt();
+//        System.out.println("초를 입력하세요");
+//        second_I = sc.nextInt();
+//        System.out.println(hour_I+":"+minute_I+":"+second_I);
+    }
 }
